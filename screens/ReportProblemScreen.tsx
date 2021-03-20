559d6a810d82a3756239516273479a6623488fdb
Corona Laufer <laufer.corona@hotmail.com>
2021-03-20 16:33:12 +0100

    fix: report problem

--- screens/ReportProblemScreen.tsx
+++ screens/ReportProblemScreen.tsx
@@ -4,13 +4,15 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function ReportProblem(): JSX.Element {
   return (
     <View style={styles.container}>
-      <Text>Report a problem</Text>
+      <Text>Hello</Text>
     </View>
   );
 }
 
+// const { height } = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#E5E5E5',
   },
 });
