ba3d9338f4c0c3675cb03ec207309c8845e1576f
Haydn <haydnmartin@gmx.com>
2021-03-20 15:35:51 +0100

    Reducer Type

--- App.tsx
+++ App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
 import reducers from './store/reducer';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { NavigationContainer } from '@react-navigation/native';
 import MainRootStack from './RootStack';
 import SafeViewAndroid from './components/SafeViewAndroid';
+import ReduxThunk from 'redux-thunk';
+import logger from 'redux-logger';
 
+const middleware = applyMiddleware(ReduxThunk, logger);
 
-const store = createStore(reducers);
+const store = createStore(reducers, middleware);
 
 export default function App () {
   return (
