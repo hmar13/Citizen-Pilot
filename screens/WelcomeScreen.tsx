ed438b6a7ab3e2bc875c7ad3f237c3313848b5e5
Corona Laufer <laufer.corona@hotmail.com>
2021-03-21 20:18:23 +0100

    fix: welcome screeen layout

--- screens/WelcomeScreen.tsx
+++ screens/WelcomeScreen.tsx
@@ -1,61 +1,43 @@
 /* eslint-disable global-require */
 import React from 'react';
-import { View, StyleSheet, Image, Dimensions, Text } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import ButtonComponent from '../components/WelcomeComponents/Button';
+import BannerComponent from '../components/BannerComponent';
+import LogoComponent from '../components/LogoComponent';
 
 export default function WelcomeScreen(): JSX.Element {
   return (
     <View style={styles.container}>
-      <View>
-        <Image
-          style={styles.banner}
-          source={require('../assets/images/banner-long.png')}
-        />
-      </View>
+      <BannerComponent />
       <View style={styles.secondColumn}>
-        <View style={styles.logoContainer}>
-          <Image
-            style={styles.logo}
-            source={require('../assets/images/logo.png')}
-          />
-        </View>
+        <LogoComponent />
         <Text style={styles.text}>Log in and make your {'\n'} voice heard</Text>
-        <ButtonComponent buttonText="Sign in" />
+        <View style={styles.buttonContainer}>
+          <ButtonComponent buttonText="Sign in" />
+        </View>
       </View>
     </View>
   );
 }
 
-const { height } = Dimensions.get('window');
-const { width } = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
     flexDirection: 'row',
   },
-  banner: {
-    height,
-    width: width / 3.6,
-  },
   secondColumn: {
     alignItems: 'center',
     justifyContent: 'center',
     marginLeft: 36,
-  },
-  logoContainer: {
-    marginTop: 120,
-    marginBottom: -50,
-  },
-  logo: {
-    resizeMode: 'contain',
-    height: 210,
-    width: 210,
   },
   text: {
     color: '#A9A9A9',
     fontSize: 15,
     textAlign: 'center',
     fontStyle: 'italic',
+  },
+  buttonContainer: {
+    marginTop: '120%',
   },
 });
