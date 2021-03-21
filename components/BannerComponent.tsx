7c911fa1ebe62c9afd650b4f4138c3556b74455a
Corona Laufer <laufer.corona@hotmail.com>
2021-03-21 17:17:20 +0100

    refactor: logo and banner component

---
+++ components/BannerComponent.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable global-require */
+import React from 'react';
+import { View, StyleSheet, Image, Dimensions } from 'react-native';
+
+export default function BannerComponent(): JSX.Element {
+  return (
+    <View>
+      <Image
+        style={styles.banner}
+        source={require('../assets/images/banner-long.png')}
+      />
+    </View>
+  );
+}
+
+const { height } = Dimensions.get('window');
+const { width } = Dimensions.get('window');
+const styles = StyleSheet.create({
+  banner: {
+    height,
+    width: width / 3.6,
+  },
+});
