cf4e5252cb7fa2ab72a5c2dcb68c3d9fb507f00e
Corona Laufer <laufer.corona@hotmail.com>
2021-03-21 20:07:53 +0100

    fix: small changes

--- RootStack.tsx
+++ RootStack.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-// import WelcomeScreen from './screens/WelcomeScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
 import Login from './screens/Login';
 import Register from './screens/Register';
 // import Contacts from './screens/Contacts';
@@ -14,7 +14,7 @@ const RootStack = createStackNavigator();
 export default function MainRootStack(): JSX.Element {
   return (
     <RootStack.Navigator>
-      {/* <RootStack.Screen name="Welcome" component={WelcomeScreen} /> */}
+      <RootStack.Screen name="Welcome" component={WelcomeScreen} />
       <RootStack.Screen name="Login" component={Login} />
       <RootStack.Screen name="Register" component={Register} />
       {/* <RootStack.Screen name='Dashboard' component={Dashboard} /> */}
--- screens/WelcomeScreen.tsx
+++ screens/WelcomeScreen.tsx
@@ -1,43 +1,61 @@
 /* eslint-disable global-require */
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Image, Dimensions, Text } from 'react-native';
 import ButtonComponent from '../components/WelcomeComponents/Button';
-import BannerComponent from '../components/BannerComponent';
-import LogoComponent from '../components/LogoComponent';
 
 export default function WelcomeScreen(): JSX.Element {
   return (
     <View style={styles.container}>
-      <BannerComponent />
+      <View>
+        <Image
+          style={styles.banner}
+          source={require('../assets/images/banner-long.png')}
+        />
+      </View>
       <View style={styles.secondColumn}>
-        <LogoComponent />
-        <Text style={styles.text}>Log in and make your {'\n'} voice heard</Text>
-        <View style={styles.buttonContainer}>
-          <ButtonComponent buttonText="Sign in" />
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={require('../assets/images/logo.png')}
+          />
         </View>
+        <Text style={styles.text}>Log in and make your {'\n'} voice heard</Text>
+        <ButtonComponent buttonText="Sign in" />
       </View>
     </View>
   );
 }
 
+const { height } = Dimensions.get('window');
+const { width } = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
     flexDirection: 'row',
   },
+  banner: {
+    height,
+    width: width / 3.6,
+  },
   secondColumn: {
     alignItems: 'center',
     justifyContent: 'center',
     marginLeft: 36,
+  },
+  logoContainer: {
+    marginTop: 120,
+    marginBottom: -50,
+  },
+  logo: {
+    resizeMode: 'contain',
+    height: 210,
+    width: 210,
   },
   text: {
     color: '#A9A9A9',
     fontSize: 15,
     textAlign: 'center',
     fontStyle: 'italic',
-  },
-  buttonContainer: {
-    marginTop: '120%',
   },
 });
