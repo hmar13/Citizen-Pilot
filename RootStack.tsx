ec02e857bb9b3acae317c5f8cb9e45583aebc8c6
Djordje <djsimovic@gmail.com>
2021-03-23 16:30:02 +0100

    merge

--- RootStack.tsx
+++ RootStack.tsx
@@ -3,10 +3,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 import WelcomeScreen from './screens/WelcomeScreen';
 import Login from './screens/Login';
 import Register from './screens/Register';
-// import Contacts from './screens/Contacts';
+import Contacts from './screens/Contacts';
 import Dashboard from './screens/Dashboard';
-// import Projects from './screens/Projects';
-// import Favourites from './screens/Favourites';
+import Projects from './screens/Projects';
+import Favourites from './screens/Favourites';
 import ProposeSolution from './screens/ProposeSolutionScreen';
 import ReportProblem from './screens/ReportProblemScreen';
 import News from './screens/News';
@@ -21,11 +21,11 @@ export default function MainRootStack(): JSX.Element {
       }}
     >
       {/* <RootStack.Screen name="Welcome" component={WelcomeScreen} /> */}
-      {/* <RootStack.Screen name="Login" component={Login} />
-      <RootStack.Screen name="Register" component={Register} /> */}
-      <RootStack.Screen name="Dashboard" component={Dashboard} />
+      {/* <RootStack.Screen name="Login" component={Login} /> */}
+      {/* <RootStack.Screen name="Register" component={Register} /> */}
+      {/* <RootStack.Screen name="Dashboard" component={Dashboard} /> */}
       {/* <RootStack.Screen name="News" component={News} /> */}
-      {/* <RootStack.Screen name='Contacts' component={Contacts} /> */}
+      {/* <RootStack.Screen name="Contacts" component={Contacts} /> */}
       {/* <RootStack.Screen name="ReportProblem" component={ReportProblem} /> */}
       {/* <RootStack.Screen name="ProposeSolution" component={ProposeSolution} /> */}
       {/* <RootStack.Screen name='CurrentProposals' component={CurrentProposals} /> */}
